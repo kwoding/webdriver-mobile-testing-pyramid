708bbd3132228fd4b1105884eed9261424f4c40d
Kwo Ding <kwo.ding@outlook.com>
2019-02-16 14:11:13 +0100

    Set up real device exercise 2

--- src/test/java/it/ding/webdriver/RealDeviceAndroidTest.java
+++ src/test/java/it/ding/webdriver/RealDeviceAndroidTest.java
@@ -6,13 +6,9 @@ import static it.ding.webdriver.Platform.NEXUS5X_RDC;
 import static it.ding.webdriver.util.BrowserUtil.takeScreenshot;
 
 import io.appium.java_client.android.AndroidDriver;
-import it.ding.webdriver.pageobject.BaseMarkPage;
 import it.ding.webdriver.pageobject.DataIqHomePage;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -22,8 +18,6 @@ public class RealDeviceAndroidTest {
     private static AndroidDriver driver;
 
     private DataIqHomePage dataIqHomePage = new DataIqHomePage(driver);
-
-    private BaseMarkPage baseMarkPage = new BaseMarkPage(driver);
 
     @BeforeClass
     public static void setUp() throws MalformedURLException {
@@ -40,35 +34,5 @@ public class RealDeviceAndroidTest {
     public void canTakeScreenshotOfScreen() throws IOException {
         dataIqHomePage.visit();
         takeScreenshot("dataiq-real-android-device");
-    }
-
-    @Test
-    public void canGetDeviceInfo() throws InterruptedException {
-        baseMarkPage.visit();
-        baseMarkPage.runDemo();
-
-        System.out.println("memoryinfo: " + getDeviceInfo(driver, "memoryinfo"));
-        System.out.println("batteryinfo: " + getDeviceInfo(driver, "batteryinfo"));
-        System.out.println("networkinfo: " + getDeviceInfo(driver, "networkinfo"));
-        Thread.sleep(30000);
-
-        System.out.println("memoryinfo: " + getDeviceInfo(driver, "memoryinfo"));
-        System.out.println("batteryinfo: " + getDeviceInfo(driver, "batteryinfo"));
-        System.out.println("networkinfo: " + getDeviceInfo(driver, "networkinfo"));
-    }
-
-    private Map<String, Integer> getDeviceInfo(AndroidDriver driver, String dataType) {
-        List<List<Object>> data = driver.getPerformanceData("com.android.chrome", dataType, 10);
-        Map<String, Integer> readableData = new HashMap<>();
-        for (int i = 0; i < data.get(0).size(); i++) {
-            int val;
-            if (data.get(1).get(i) == null) {
-                val = 0;
-            } else {
-                val = Integer.parseInt((String) data.get(1).get(i));
-            }
-            readableData.put((String) data.get(0).get(i), val);
-        }
-        return readableData;
     }
 }
