1f6b4c6c6e12feb9ec68773b318d5ff85e2c6243
Kwo Ding <kwo@ding.it>
2019-02-16 14:26:17 +0100

    Set up desktop exercise 2

--- src/test/java/it/ding/webdriver/DesktopBrowserTest.java
+++ src/test/java/it/ding/webdriver/DesktopBrowserTest.java
@@ -4,20 +4,16 @@ import static it.ding.webdriver.DriverFactory.getDriver;
 import static it.ding.webdriver.DriverFactory.setDriver;
 import static it.ding.webdriver.Platform.CHROME_DESKTOP_LOCAL;
 import static it.ding.webdriver.util.BrowserUtil.resizeBrowser;
-import static it.ding.webdriver.util.BrowserUtil.takeScreenshot;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import it.ding.webdriver.pageobject.BookingComPage;
 import it.ding.webdriver.pageobject.TheInternetLoginPage;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.html5.Location;
-import org.openqa.selenium.html5.LocationContext;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class DesktopBrowserTest {
@@ -55,13 +51,5 @@ public class DesktopBrowserTest {
         bookingComPage.visit();
         resizeBrowser(360, 740);
         assertThat(bookingComPage.isBookingAppDisplayed(), is(true));
-    }
-
-    @Test
-    public void canSetGeoLocation() throws InterruptedException, IOException {
-        ((LocationContext) driver).setLocation(new Location(3.1466, 101.6958, 100));
-        driver.get("http://maps.google.com");
-        Thread.sleep(3000);
-        takeScreenshot("gps");
     }
 }
